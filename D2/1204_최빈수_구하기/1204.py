e9be14e0fca39ea7ddc34e39e111cbf70f1c8620
SunM <coex0113@gmail.com>
2025-05-19 17:24:49 +0900

    Feat: 1204 py 풀이 코드 추가
    1204.py 파일 추가

---
+++ D2/1204_최빈수_구하기/1204.py
@@ -0,0 +1,15 @@
+from collections import defaultdict
+
+T = int(input())
+
+for test_case in range(1, T + 1):
+    _ = int(input())
+    arr = list(map(int, input().split()))
+
+    dd = defaultdict(int)
+    
+    for el in arr:
+        dd[el] += 1
+    
+    sorted_dd = sorted(dd.items(), key = lambda x : x[1], reverse = True)
+    print(f"#{test_case} {sorted_dd[0][0]}")
